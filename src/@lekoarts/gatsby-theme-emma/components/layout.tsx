67a15355d571aa949911f7111e401f0c3861353c
unknown <jyskwon@umich.edu>
2025-05-09 18:22:41 +0900

    initial version of homepage

---
+++ src/@lekoarts/gatsby-theme-emma/components/layout.tsx
@@ -0,0 +1,54 @@
+import * as React from "react"
+import { Global } from "@emotion/react"
+import { Box, get } from "theme-ui"
+import useSiteMetadata from "../hooks/use-site-metadata"
+import useNavigation from "../hooks/use-navigation"
+import Footer from "./footer"
+import Header from "./header"
+
+
+
+import NewsContent from "./NewsContent"
+import { useLocation } from "@reach/router"
+
+
+
+type LayoutProps = { children: React.ReactNode; className?: string }
+
+const Layout = ({ children, className = `` }: LayoutProps) => {
+  const meta = useSiteMetadata()
+  const nav = useNavigation()
+
+  return (
+    <React.Fragment>
+      <Global
+        styles={(t) => ({
+          "*": {
+            boxSizing: `inherit`,
+          },
+          "[hidden]": {
+            display: `none`,
+          },
+          "::selection": {
+            backgroundColor: get(t, `colors.text`),
+            color: get(t, `colors.background`),
+          },
+        })}
+      />
+      <Header meta={meta} nav={nav} />
+      <Box as="main" variant="layout.main" className={className}>
+        {children}
+      </Box>
+
+
+
+      {location.pathname === "/" && <NewsContent />}
+
+
+
+      <Footer />
+    </React.Fragment>
+  )
+}
+
+export default Layout
